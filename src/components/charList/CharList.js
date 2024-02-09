816ac2f32bf7ac16a5be1a5e9df180eff17fd8d8
Serhii <kindratsergij60@gmail.com>
2024-02-09 19:06:27 +0200

    feat: start working with animation, download basic pachages

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/spinner';
